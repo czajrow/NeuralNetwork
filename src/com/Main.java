e81b2d0db1ffac96713a3ed03a68e1baf452df76
czajkaa1 <czajka.adam147@gmail.com>
2018-12-19 23:14:50 +0100

    metoda train() w klasie NeuralNetwork, w main() rozwiazany problem XOR

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,8 +1,46 @@
 package com;
 
+import com.nn.NeuralNetwork;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        NeuralNetwork neuralNetwork = new NeuralNetwork(2, 8, 1);
+
+        double[] inputs1 = {0, 0};
+        double[] inputs2 = {0, 1};
+        double[] inputs3 = {1, 0};
+        double[] inputs4 = {1, 1};
+
+        double[] outputs1 = {0};
+        double[] outputs2 = {1};
+        double[] outputs3 = {1};
+        double[] outputs4 = {0};
+
+        for (int i = 0; i < 100000; i++) {
+            neuralNetwork.train(inputs1, outputs1);
+            neuralNetwork.train(inputs2, outputs2);
+            neuralNetwork.train(inputs3, outputs3);
+            neuralNetwork.train(inputs4, outputs4);
+        }
+
+        double[] guess1 = neuralNetwork.feedforward(inputs1);
+        double[] guess2 = neuralNetwork.feedforward(inputs2);
+        double[] guess3 = neuralNetwork.feedforward(inputs3);
+        double[] guess4 = neuralNetwork.feedforward(inputs4);
+
+
+        System.out.format("target1= %.3g\n", outputs1[0]);
+        System.out.format("guess1= %.3g\n\n", guess1[0]);
+
+        System.out.format("target1= %.3g\n", outputs2[0]);
+        System.out.format("guess2= %.3g\n\n", guess2[0]);
+
+        System.out.format("target1= %.3g\n", outputs3[0]);
+        System.out.format("guess3= %.3g\n\n", guess3[0]);
+
+        System.out.format("target1= %.3g\n", outputs4[0]);
+        System.out.format("guess4= %.3g\n\n", guess4[0]);
     }
 }
--- src/com/nn/NeuralNetwork.java
+++ src/com/nn/NeuralNetwork.java
@@ -44,6 +44,62 @@ public class NeuralNetwork {
         return guess(inputArray).toArray();
     }
 
+    public void train(double[] inputArray, double[] targetArray) {
+
+        // generating the hidden outputs
+        Matrix inputs = Matrix.fromArray(inputArray);
+        Matrix hidden = Matrix.multiply(weights_IH, inputs);
+        hidden.add(bias_H);
+
+        // activation function
+        hidden.map(sigmoid);
+
+        // generating the output's output
+        Matrix outputs = Matrix.multiply(weights_HO, hidden);
+        outputs.add(bias_O);
+        outputs.map(sigmoid);
+
+        // Convert array to matrix object
+        Matrix targets = Matrix.fromArray(targetArray);
+
+        // calculate the error
+        // ERROR = TARGETS - OUTPUTS
+        Matrix outputErrors = Matrix.subtract(targets, outputs);
+
+        // calculate gradient
+        Matrix gradients = Matrix.map(outputs, dsigmoid);
+        gradients.multiply(outputErrors);
+        gradients.multiply(learningRate);
+
+        // calculate deltas
+        Matrix hidden_T = Matrix.transpose(hidden);
+        Matrix weight_HO_deltas = Matrix.multiply(gradients, hidden_T);
+
+        // adjust the weights by deltas
+        weights_HO.add(weight_HO_deltas);
+
+        // Adjust the bias by its deltas (which is just the gradients)
+        bias_O.add(gradients);
+
+        // Calculate the hidden layer errors
+        Matrix who_T = Matrix.transpose(weights_HO);
+        Matrix hiddenErrors = Matrix.multiply(who_T, outputErrors);
+
+        // Calculate hidden gradient
+        Matrix hiddenGradient = Matrix.map(hidden, dsigmoid);
+        hiddenGradient.multiply(hiddenErrors);
+        hiddenGradient.multiply(learningRate);
+
+        // Calcuate input->hidden deltas
+        Matrix inputs_T = Matrix.transpose(inputs);
+        Matrix weight_IH_deltas = Matrix.multiply(hiddenGradient, inputs_T);
+
+        weights_IH.add(weight_IH_deltas);
+
+        // adjust the bias by its deltas (which is just the gradients)
+        bias_H.add(hiddenGradient);
+    }
+
     private Matrix guess(double[] inputArray) {
 
         // generating the hidden outputs
