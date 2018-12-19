ed7cbd44c3df9a38153fa9561da5303b3164118d
czajkaa1 <czajka.adam147@gmail.com>
2018-12-19 12:20:59 +0100

    .idea/

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         // todo TESTY Matrixa
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(new double);
+        }
     }
 }
